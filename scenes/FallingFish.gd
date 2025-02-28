3a98dc58884d8e564e1087681b54b0660bc60f8a
TehGaa <syosuasaputra1010@gmail.com>
2025-02-28 11:41:05 +0700

    feat: add latihan from tutorial 4

---
+++ scenes/FallingFish.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+@export var sceneName = "LoseScreen"
+
+func _on_FallArea_body_entered(body):
+	if body.get_name() == "Player":
+		get_tree().change_scene_to_file(str("res://scenes/" + sceneName + ".tscn"))
+	elif body.get_name() == "FallingFish":
+		pass
+	else:
+		get_parent().queue_free()
